ce2b5c10c3a71ef48ca5d646b35adf73db86cc5d
Gavin Bauer <gb1035@gmail.com>
2017-02-05 17:41:49 -0600

    fixed insert

--- set_request.php
+++ set_request.php
@@ -19,7 +19,7 @@ if ($conn->connect_error) {
 }
 
 echo "Connected successfully";
-$sql = "INSERT INTO request (user_name, email, support_line, details) VALUES (" . $_POST["uname"] . ", " . $_POST["email"] . ", " . $_POST["support_line"] . ", " . $_POST["details"] . ");";
+$sql = "INSERT INTO request (user_name, email, support_line, details) VALUES ('" . $_POST["uname"] . "', '" . $_POST["email"] . "', '" . $_POST["support_line"] . "', '" . $_POST["details"] . "');";
 echo $sql;
 if ($conn->query($sql) === TRUE) {
     echo "<h1>Your ticket has been submitted successfully!</h1>";
