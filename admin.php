15c0bc30e3b580efebb0a75f4ccb4e1355d219ca
Gavin Bauer <gb1035@gmail.com>
2017-02-05 18:17:56 -0600

    .

--- admin.php
+++ admin.php
@@ -1,11 +1,11 @@
 <?php
-
+echo $_COOKIE;
 if (isset($_COOKIE['username'])) {
 	echo "Welcome admin!";
 	echo $_COOKIE['username'];
 }
 else{
-	header( 'Location: /login.php' ) ;
+	// header( 'Location: /login.php' ) ;
 }
 
 ?>
