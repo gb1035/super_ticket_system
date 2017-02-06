8247f72cc301e5d66a0be92ec93afb51b2166a6b
Gavin Bauer <gb1035@gmail.com>
2017-02-05 18:34:50 -0600

    .

--- admin.php
+++ admin.php
@@ -22,7 +22,7 @@ if (isset($_COOKIE['username'])) {
 		if ($result->num_rows > 0) {
 		    // output data of each row
 		    while($row = $result->fetch_assoc()) {
-		        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+		        echo "id: " . $row["id"]. " - Name: " . $row["user_name"]. " " . $row["email"]. " " .$row["support_line"]. " ". $row["details"] ."<br>";
 		    }
 		} else {
 		    echo "0 results";
