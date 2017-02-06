46bcead52a1ac22c89e7ef0daddb51c9edfa2dd6
Gavin Bauer <gb1035@gmail.com>
2017-02-05 19:43:50 -0600

    .

--- admin.php
+++ admin.php
@@ -21,9 +21,11 @@ if (isset($_COOKIE['username'])) {
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 		    // output data of each row
+		    echo "<table><tr><th>ID</th><th>User Name</th><th>Email</th><th>Title</th><th>Details</th></tr>";
 		    while($row = $result->fetch_assoc()) {
-		        echo "id: " . $row["id"]. " - Name: " . $row["user_name"]. " " . $row["email"]. " " .$row["support_line"]. " ". $row["details"] ."<br>";
+		        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["user_name"]. "</td><td>" . $row["email"]. "</td><td>" .$row["support_line"]. "</td><td>". $row["details"] ."</td></tr>";
 		    }
+		    echo "</table>"
 		} else {
 		    echo "0 results";
 		}
